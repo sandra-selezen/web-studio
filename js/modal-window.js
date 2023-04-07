24965b45cc92e4c5aa69150c7ebdacf6cfbabc80
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-04-07 18:27:43 +0300

    Update script for modal window

--- js/modal-window.js
+++ js/modal-window.js
@@ -3,6 +3,7 @@ const refs = {
   closeModalBtn: document.querySelector("[data-modal-close]"),
   modal: document.querySelector("[data-modal]"),
   backdrop: document.querySelector(".backdrop"),
+  body:document.querySelector("body"),
 };
 
 refs.openModalBtn.addEventListener("click", onOpenModalBtnClick);
@@ -11,12 +12,14 @@ refs.backdrop.addEventListener("click", onBackdropClick);
 
 function onOpenModalBtnClick() {
   refs.modal.classList.remove("is-hidden");
+  refs.body.classList.add("modal-open");
   window.addEventListener("keydown", onEscKeyDownModal);
 };
 
 function onCloseModalBtnClick() {
   window.removeEventListener("keydown", onEscKeyDownModal);
   refs.modal.classList.add("is-hidden");
+  refs.body.classList.remove("modal-open");
 };
 
 function onBackdropClick(event) {
